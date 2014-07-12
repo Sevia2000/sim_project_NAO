cb59d315b3662857a6e23428027b57a88651ff3c
nathanielmendoza <namendoz@asu.edu>
2014-07-11 17:34:44 -0700

    + RMI server tweaks, door push tweak

--- controllers/SupervisorController/SupervisorController.java
+++ controllers/SupervisorController/SupervisorController.java
@@ -16,6 +16,8 @@ import com.cyberbotics.webots.controller.Field;
 
 import java.awt.geom.Point2D;
 
+import java.util.Scanner;
+
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -31,14 +33,12 @@ import java.lang.Math;
 // Note that this class derives Robot and so inherits all its functions
 public class SupervisorController extends Supervisor {
   
-  // You may need to define your own functions or variables, like
-  //  private LED led;
-  private double x = 0;
-  private double y = 0;
-  private double a = 0;
-  private static RMIServer obj = null;
+	// You may need to define your own functions or variables, like
+	//  private LED led;
+
+	private static RMIServer obj = null;
   
-  // SupervisorController constructor
+	// SupervisorController constructor
   public SupervisorController() {
       
     // call the Robot constructor
@@ -68,56 +68,66 @@ public class SupervisorController extends Supervisor {
  
     while (step(128) != -1) {
       
-      // Read the sensors:l
-      // Enter here functions to read sensor data, like:
-      //  double val = distanceSensor.getValue();
-      double[] trans = trans_field.getSFVec3f();
-      //double[] rotation = rot_field.getSFRotation();
-      // Process sensor data here
-      //System.out.println("My robot is at position " + trans[0] + ", " + trans[1] + ", " + trans[2]);
-      x = trans[0];
-      y = trans[2];
-      
-      obj.x = trans[0];
-      obj.y = trans[2];
-     
-      double[] rotation = robot_node.getOrientation();
-      double angle = Math.atan2(rotation[0], rotation[6]);
-      // rounding (double)Math.round(value * 100000) / 100000
+		// Read the sensors:l
+		// Enter here functions to read sensor data, like:
+		//  double val = distanceSensor.getValue();
+		double[] trans = trans_field.getSFVec3f();
+		//double[] rotation = rot_field.getSFRotation();
+		// Process sensor data here
+		//System.out.println("My robot is at position " + trans[0] + ", " + trans[1] + ", " + trans[2]);
+
+		obj.x = trans[0];
+		obj.y = trans[2];
+
+		double[] rotation = robot_node.getOrientation();
+		double angle = Math.atan2(rotation[0], rotation[6]);
+		// rounding (double)Math.round(value * 100000) / 100000
     
       
-      angle = (angle > 0 ? angle : (2*Math.PI + angle)) * 360 / (2*Math.PI);
+		angle = (angle > 0 ? angle : (2*Math.PI + angle)) * 360 / (2*Math.PI);
+
+		//(x > 0 ? x : (2*PI + x)) * 360 / (2*PI)
+
+		//change axis orientation so 0 is at X axis
+		angle = angle - 90;
+
+		if(angle < 0)
+			angle = angle + 360;
       
-      //(x > 0 ? x : (2*PI + x)) * 360 / (2*PI)
+		angle = (double)Math.round(angle * 1000) / 1000;
       
-      //change axis orientation so 0 is at X axis
-      angle = angle - 90;
+		if(angle == 360)
+			angle = 0;
+
+		obj.angle = angle;
       
-      if(angle < 0)
-        angle = angle + 360;
-      
-      
-      angle = (double)Math.round(angle * 1000) / 1000;
-      
-      if(angle == 360)
-        angle = 0;
-      
-      a = angle;
+		//PROCESS COMMANDS HERE
+		if(prevSize != obj.commands.size())
+		{
+			prevSize = obj.commands.size();
+			String lastcommand = obj.commands.get(prevSize - 1);
+			System.out.println(lastcommand);
+			
+			Scanner scan = new Scanner(lastcommand);
+			String move = scan.next();
+			switch(move.toLowerCase())
+			{
+			    case "push":
+					Node doorNode = this.getFromDef(scan.next());
+					Field params = doorNode.getField("position");
+					params.setSFFloat(1.8326);
+				break;
+				
+				default:
+			
+			}
+			
+		}
      
-      obj.angle = angle;
-      
-      //PROCESS COMMANDS HERE
-      if(prevSize != obj.commands.size())
-      {
-          prevSize = obj.commands.size();
-          String lastcommand = (String) obj.commands.get(prevSize - 1);
-          System.out.println(lastcommand);
-      }
-     
-     //System.out.println("The robot's rotation: " + angle);
-      
-      // Enter here functions to send actuator commands, like:
-      //  led.set(1);
+		//System.out.println("The robot's rotation: " + angle);
+
+		// Enter here functions to send actuator commands, like:
+		//  led.set(1);
     };
     
     // Enter here exit cleanup code
@@ -135,27 +145,28 @@ public class SupervisorController extends Supervisor {
    
     
     System.out.println("RMI server started**********************************************************");
-    System.out.println("****************************************************************************");
-     System.out.println("****************************************************************************");
-      System.out.println("****************************************************************************");
-       System.out.println("****************************************************************************");
-        System.out.println("****************************************************************************");
-        try { //special exception handler for registry creation
-            LocateRegistry.createRegistry(1099); 
-            System.out.println("java RMI registry created.");
-        } catch (Exception e) {
-            //do nothing, error means registry already exists
-            System.out.println("java RMI registry already exists.");
-        }
+	System.out.println("****************************************************************************");
+	System.out.println("****************************************************************************");
+	System.out.println("****************************************************************************");
+	System.out.println("****************************************************************************");
+	System.out.println("****************************************************************************");
+	
+	try { //special exception handler for registry creation
+		LocateRegistry.createRegistry(1099); 
+		System.out.println("java RMI registry created.");
+	} catch (Exception e) {
+		//do nothing, error means registry already exists
+		System.out.println("java RMI registry already exists.");
+	}
  
-        //Instantiate RmiServer
-        obj = new RMIServer();
- 
-        // Bind this object instance to the name "RmiServer"
-        Naming.rebind("//localhost/RmiServer", obj);
-        System.out.println("PeerServer bound in registry");
+	//Instantiate RmiServer
+	obj = new RMIServer();
+
+	// Bind this object instance to the name "RmiServer"
+	Naming.rebind("//localhost/RmiServer", obj);
+	System.out.println("PeerServer bound in registry");
     
-     SupervisorController controller = new SupervisorController();
+	SupervisorController controller = new SupervisorController();
     controller.run();
   }
 }
