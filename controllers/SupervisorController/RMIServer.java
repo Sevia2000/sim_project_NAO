c9d8247a00057f61e0f13f91b47a9b60f9549501
nathanielmendoza <namendoz@asu.edu>
2014-07-16 17:14:28 -0700

    + preliminary reporting code

--- controllers/SupervisorController/RMIServer.java
+++ controllers/SupervisorController/RMIServer.java
@@ -3,6 +3,12 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.*; 
 
+import java.util.Map;
+import java.util.HashMap;
+
+import java.util.Random;
+import java.security.SecureRandom;
+
 import java.util.ArrayList;
 
 import java.awt.geom.Point2D;
@@ -12,12 +18,49 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerIntf
     public double x = 0;
     public double y = 0;
     public double angle = 0;
+    public int[] roomCasualties = new int[8];
     
+    private static final int BOX_COUNT = 50;
+    
+    public Map<String, Integer> casualties = new HashMap<String, Integer>();
+ 
     public ArrayList<String> commands = new ArrayList<String>();
     
     public RMIServer() throws RemoteException
     {
+        //out of 100
+   
+    }
     
+    public void generate()
+    {
+        SecureRandom rand = new SecureRandom();
+        
+        for(int i = 0; i < 8; i++)
+        {
+            roomCasualties[i] = 0;
+        }
+        
+        for(int i = 0; i < BOX_COUNT; i++)
+        {
+            byte[] bytes = new byte[20];
+            rand.setSeed(bytes);
+            
+            int gen = rand.nextInt(100) + 1;
+            
+            if(gen <= 5)
+            {
+                roomCasualties[0] += 1;
+            }
+            else if(gen <= 15)
+            {
+            
+            }
+            else if(gen <= )
+            {
+            
+            }
+        }
     }
     
     public double getAngle()
@@ -33,6 +76,11 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerIntf
     public void addCommand(String command)
     {
         commands.add(command);
+    }
+    
+    public int getCasualty(String room)
+    {
+        return casualties.get(room).intValue();
     }
     
 
--- controllers/SupervisorController/RMIServerIntf.java
+++ controllers/SupervisorController/RMIServerIntf.java
@@ -6,6 +6,6 @@ public interface RMIServerIntf extends Remote {
     public double getAngle() throws RemoteException;
      public Point2D.Double getLocation() throws RemoteException;
      public void addCommand(String command) throws RemoteException;
-    
+    public int getCasualty(String codename) throws RemoteException;
     
 }
